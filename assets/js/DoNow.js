f8900468c5fe1e8b82880fa35271de403d3dab8d
Dalia <lovablechic77@aim.com>
2018-12-02 18:14:56 -0500

    changed masthead for do-now with new image and played around with columns for quotes

--- assets/js/DoNow.js
+++ assets/js/DoNow.js
@@ -6,6 +6,9 @@ function newQuote() {
 var quotes = [
 	'<b>Journal for 10 minutes</b><br>Answer three questions: How am I feeling right now? What brought me absolute delight in the past 24hrs? What are the patterns?',
 	'<b>1-minute dance party</b><br>Ready, set, go.',
+	'Floss - you will be SO glad you did',
+	'Clean up one section of your room'.
+	'Message 5 friends you haven\'t spoken to in a while',
 	'Test 1',
 	'Test 2'
 	]
