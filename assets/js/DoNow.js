443541713ba34e63c795cc214dc5cfa50e1dd523
Dalia <lovablechic77@aim.com>
2018-12-02 18:34:06 -0500

    new js still not updated on site - updated quote content

--- assets/js/DoNow.js
+++ assets/js/DoNow.js
@@ -6,9 +6,9 @@ function newQuote() {
 var quotes = [
 	'<b>Journal for 10 minutes</b><br>Answer three questions: How am I feeling right now? What brought me absolute delight in the past 24hrs? What are the patterns?',
 	'<b>1-minute dance party</b><br>Ready, set, go.',
-	'Floss - you will be SO glad you did',
-	'Clean up one section of your room',
-	'Message 5 friends you have not spoken to in a while',
+	'<b>Floss</b><br>You will be SO glad you did',
+	'<b>Clean up one section of your room</b><br>Why not start with your desk, that pile of laundry on your couch, or your wallet?',
+	'Message 3 friends you have not spoken to in a while',
 	'Test 1',
 	'Test 2'
 	]
