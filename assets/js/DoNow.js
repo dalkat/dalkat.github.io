c8f456e3960b4a88281f0a3695b87915b8bf11e3
Dalia <lovablechic77@aim.com>
2018-12-02 17:54:19 -0500

    put do now into default-sub and added some quote content

--- assets/js/DoNow.js
+++ assets/js/DoNow.js
@@ -1,11 +1,11 @@
 function newQuote() {
-	var randomNumber = Math.floor(Math.random() * (quotes.length));
+	var randomNumber = Math.floor(Math.random() * ((quotes.length)+1));
 	document.getElementById('quotesDisplay').innerHTML = quotes[randomNumber];
 	}
 
 var quotes = [
-	'A',
-	'B',
-	'C',
-	'D'
+	'<b>Journal for 10 minutes</b><br>Answer three questions: How am I feeling right now? What brought me absolute delight in the past 24hrs? What are the patterns?',
+	'<b>1-minute dance party</b><br>Ready, set, go.',
+	'Test 1',
+	'Test 2'
 	]
