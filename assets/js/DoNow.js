5c61c0eb09c3f7d68b9343ac92048c53cbadfa01
Dalia <lovablechic77@aim.com>
2018-12-02 19:06:35 -0500

    button color

--- assets/js/DoNow.js
+++ assets/js/DoNow.js
@@ -10,6 +10,8 @@ var quotes = [
 	'<b>Clean up one section of your room</b><br>Why not start with your desk, that pile of laundry on your couch, or your wallet?',
 	'Message 3 friends you have not spoken to in a while',
 	'<b>Take care of your skin</b><br>Make a face mask, exfoliate your body, or lotion up!',
+	'<b>Random act of kindness</b><Br>What could you do right now, big or small?',
+	'<b>Write a haiku</b><br>How about an ode to the weather, gratitude, or about what you had/are having for lunch?',
 	'Test 1',
 	'Test 2'
 	]
