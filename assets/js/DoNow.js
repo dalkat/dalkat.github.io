95ff2a14cc6a6b73de7537a18fcc846b32e51eab
Dalia <lovablechic77@aim.com>
2018-12-02 19:13:21 -0500

    removed test quotes

--- assets/js/DoNow.js
+++ assets/js/DoNow.js
@@ -11,7 +11,5 @@ var quotes = [
 	'Message 3 friends you have not spoken to in a while',
 	'<b>Take care of your skin</b><br>Make a face mask, exfoliate your body, or lotion up!',
 	'<b>Random act of kindness</b><Br>What could you do right now, big or small?',
-	'<b>Write a haiku</b><br>How about an ode to the weather, gratitude, or about what you had/are having for lunch?',
-	'Test 1',
-	'Test 2'
+	'<b>Write a haiku</b><br>How about an ode to the weather, gratitude, or about what you had/are having for lunch?'
 	]
