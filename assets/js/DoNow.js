7f422ee80736f48db4df408a4706626df589f6bf
Dalia <lovablechic77@aim.com>
2018-12-03 12:12:38 -0500

    update donow content

--- assets/js/DoNow.js
+++ assets/js/DoNow.js
@@ -12,5 +12,8 @@ var quotes = [
 	'<b>Take care of your skin</b><br>Make a face mask, <a href="https://www.amazon.com/gp/product/B001ET7HXO/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B001ET7HXO&linkCode=as2&tag=dalkat-20&linkId=e650bf14d81c9fc867a57a2045ee322c" target="_blank">exfoliate</a> your body, or lotion up!',
 	'<b>Random act of kindness</b><Br>What could you do right now, big or small?',
 	'<b>Write a haiku</b><br>How about an ode to the weather, gratitude, or about what you had/are having for lunch?',
-	'<b>Take a 20-minute class on <a href="https://skl.sh/2TE2fKn" target="_blank">Skillshare</a></b><br>How about food photography, logo design, or freelance business development?'
+	'<b>Take a 20-minute learning break</b><br>How about an online class on food photography or logo design on <a href="https://skl.sh/2TE2fKn" target="_blank">Skillshare</a>, or a podcast?',
+	'<b>Do a 20 minute yoga flow</b><br>Have more time? See if there’s a class happening now at the gym!',
+	'<b>Stretch for five minutes</b><br>Yes, for five minutes! Stretch your neck, roll your shoulders, touch your toes.',
+	'<b>Meditate</b><br>A five minute meditation on SimpleHabit or Headspace.'
 	]
