6cc47d03afe5892803347c03fadb378fa56fcef7
Dalia <lovablechic77@aim.com>
2019-02-09 23:46:51 -0800

    hide subhead on testimonials

--- assets/js/clientcarousel.js
+++ assets/js/clientcarousel.js
@@ -22,14 +22,14 @@ $(document).ready(function(){
         breakpoint: 600,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToScroll: 2
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       }
       // You can unslick at a given breakpoint now by adding:
