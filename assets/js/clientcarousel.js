663becf2bf337a803f5e075916ad303dbf689e24
Dalia <lovablechic77@aim.com>
2019-02-16 12:56:52 -0800

    tweak js

--- assets/js/clientcarousel.js
+++ assets/js/clientcarousel.js
@@ -51,26 +51,6 @@ $(document).ready(function(){
     variableWidth: true,
     adaptiveHeight: true,
     slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
   });
 });
 
